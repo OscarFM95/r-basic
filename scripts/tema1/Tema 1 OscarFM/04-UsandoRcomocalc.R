620a2259f2b3d4584afd83f38ef0c67ed670ddc7
OscarFM95 <79666590+OscarFM95@users.noreply.github.com>
2021-02-28 19:38:32 -0600

    mis apuntes

---
+++ scripts/tema1/Tema 1 OscarFM/04-UsandoRcomocalc.R
@@ -0,0 +1,41 @@
+6*pi
+NaN #Not a number
+NA #Sin registro de dato
+Inf #Infinito
+7%/%3 #División con número entero
+7/3 #División normal
+7%%3 #Residuo de la división
+703%/%7 #Número de semanas en 703 días
+703%%7 #Días que sobran en las 100 semanas
+703-(100*7) #Da de resultado el resto
+5/0 #Da infinito
+0/0 #Da NaN
+2^50 #Usa notación científica para números grandes
+sqrt(9) #Para raices cuadradas
+exp(1) #Euler
+log(pi) #logaritmo neperiano
+log10(1) #logaritmo base 10
+log(1,10) #Otra forma de poner el logaritmo base 10
+abs(-10) #Devuelve el valor absoluto
+factorial(3) #El 3! (factorial)
+choose(5, 3) #Coeficiente binomial (proba) forma de
+#si queremos sacar m elementos de n, de cuantas
+#formas podemos sacarlos en el ejemplo, si tuviera
+#5 platos y quiero comerme 3 como lo puedo hacer
+6^log(4,6) #6 elevado a la logaritmo 6 de 4
+log(4^6,4) #Logaritmo de base 4 de 4^6
+sin(pi/2)
+cos(pi)
+tan(0)
+#Para calcular las funciones trigonométricas debemos
+#de pasar los número a radianes
+cos(60*pi/180)
+sinpi(1/2) #Es los mismo de sin(pi/2)
+acos(0.5) #arcocoseno en radianes 
+1.047198*180/pi #para que de el resultado en grados
+print(sqrt(2), 10) #Devuelve 10 dígitos
+round(sqrt(2), 3) #Redondea a 3 cifras signf.
+floor(sqrt(2)) #Redondea a la cifra menor entera.
+ceiling(sqrt(2)) #Redondea al entero mayor
+sqrt(2)^2-2 #No da cero por no redondear
+floor(sqrt(2)^2)-2 #Así daría 0
---
+++ scripts/tema1/Tema 1 OscarFM/05-Variablesyfunciones.R
@@ -0,0 +1,11 @@
+x = (pi^2)/2 #Así se define una variable que aparecerá
+#En enviroment
+
+y <- cos(pi/4) #Otra forma de definir variable
+
+sin(pi/4) + cos(pi/4) -> z #Otra forma def var
+
+#A lo que apunta la flecha es el nombre de variable
+
+Nombre = "Oscar" #Para definir strings entre comillas
+#Tratar de no usar caracteres especiales en variables
